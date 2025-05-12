843935c8fc77ed944b97c0a45e69a3d52cbc0e24
Théo Killian <theo.killian5700@Gmail.com>
2025-05-12 13:13:45 +0200

    Modificiation config unicorn

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from search import Search
 from fastapi.middleware.cors import CORSMiddleware
+import unicorn
 
 s = Search()
 app = FastAPI()
@@ -9,3 +10,6 @@ app = FastAPI()
 @app.get("/search/v1/{query}")
 async def search(query: str):
     return s.search(query)
+
+if __name__ == "__main__":
+    unicorn.run(app, host="0.0.0.0", port=8000)
