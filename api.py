970b841805199eb0e75f5b95ab1d7e6ff5242ed3
Théo Killian <theo.killian5700@Gmail.com>
2025-05-13 10:13:54 +0200

    Changement de l'url pour des query

--- api.py
+++ api.py
@@ -7,9 +7,9 @@ import uvicorn
 s = Search()
 app = FastAPI()
 
-@app.get("/v1/search?q={query}")
-async def search(query: str):
-    return s.search(query)
+@app.get("/v1/search")
+async def search(q: str):
+    return s.search(q)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=7000)
